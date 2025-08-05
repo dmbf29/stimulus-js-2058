ba24a4171ebb6a8e11c768a9009440f3a8f30b65
Douglas Berkley <douglasmberkley@gmail.com>
2025-08-06 08:39:17 +0900

    lecture starting point

---
+++ javascript/application.js
@@ -0,0 +1,9 @@
+// //////////////
+// Stimulus 101
+// //////////////
+
+// define what HTML will use JS => data-controller  C
+// addEventListener()           => data-action      A
+// querySelector()              => data-xxx-target  T
+
+// Bonus: data-xxx-valname-value and this.element
